d1b34fa33973ff4fcb42fd28f3dfb59279487562
Meghna2395 <66842750+Meghna2395@users.noreply.github.com>
2021-08-07 00:27:30 +0530

    Add files via upload

---
+++ MoodAnalyser.java
@@ -0,0 +1,30 @@
+package com.bridzelabz;
+
+import java.util.Scanner;
+
+public class MoodAnalyser {
+
+	public String message;
+
+	public MoodAnalyser() {
+
+	}
+
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() throws MoodAnalysisException {
+		try {
+			if (message.length() == 0)
+				throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,
+						"Please enter proper mood");
+			if (message.contains("Sad"))
+				return "SAD";
+			return "HAPPY";
+		} catch (NullPointerException e) {
+			throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL,
+					"Please enter proper mood");
+		}
+	}
+}
